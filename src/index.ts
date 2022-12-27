e3c0f929aadeea27a01c46b8fb73cc438ebd41e0
dtinth on MBP M1 <dtinth@spacet.me>
2022-12-27 23:38:02 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+// Adapted from https://github.com/iconify/iconify/blob/3fe91ddea3f421832a20aa5cf8d8ba4dfcbbe8f8/iconify-icon/react/src/iconify.ts
+// License: https://github.com/iconify/iconify/blob/3fe91ddea3f421832a20aa5cf8d8ba4dfcbbe8f8/iconify-icon/react/license.txt
+
+/* eslint-disable react/display-name */
+import React from 'react'
+
+/**
+ * Properties for React component
+ */
+export interface Icon extends React.HTMLProps<HTMLElement> {
+  icon: string | { body: string }
+  mode?: 'style' | 'mask' | 'svg' | 'bg'
+  inline?: boolean
+  width?: string | number
+  height?: string | number
+  rotate?: string | number
+  flip?: string
+}
+
+/**
+ * React component
+ */
+export const Icon = React.forwardRef(
+  (props: Icon, ref: React.ForwardedRef<HTMLElement>) => {
+    const newProps: Record<string, unknown> = {
+      ...props,
+      ref,
+    }
+
+    // Stringify icon
+    if (typeof props.icon === 'object') {
+      newProps.icon = JSON.stringify(props.icon)
+    }
+
+    // Boolean
+    if (!props.inline) {
+      delete newProps.inline
+    }
+
+    // React cannot handle className for web components
+    if (props.className) {
+      newProps['class'] = props.className
+    }
+
+    return React.createElement('iconify-icon', newProps)
+  },
+)
+
+export type { IconifyIcon } from 'iconify-icon'
